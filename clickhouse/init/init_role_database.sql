24a9edc48bff4a71a081fdc8476824c25cbef0df
ItsLewis-DE <thanhphong1110n@gmail.com>
2026-07-23 15:45:16 +0200

    dbt

---
+++ clickhouse/init/init_role_database.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS fraudguard_staging;
+CREATE DATABASE IF NOT EXISTS fraudguard_intermediate;
+CREATE DATABASE IF NOT EXISTS fraudguard_mart;
+CREATE DATABASE IF NOT EXISTS fraudguard_monitoring;

e0821293d61ed085670162e018f53c77952f428f
ItsLewis-DE <thanhphong1110n@gmail.com>
2026-08-26 05:43:45 +0200

    chore: stop tracking ML artifacts

--- airflow_ml_artifacts/model_experiments/20260825T082110Z/shared/dbt_target/compiled/fraudguard/tests/assert_no_orphan_final_labels.sql
+++
@@ -1,9 +0,0 @@
---Kiểm tra việc có trong label nhưng lại k có transaction
-select
-    l.source,
-    l.event_id
-from `fraudguard_core`.`fct_transaction_labels` as l
-left join `fraudguard_core`.`fct_transactions` as t
-    on l.source = t.source
-   and l.event_id = t.event_id
-where t.event_id = ''
--- airflow_ml_artifacts/model_experiments/20260825T082223Z/shared/dbt_target/compiled/fraudguard/models/marts/ml/ml_training_exclusion_summary.sql
+++
@@ -1,5 +0,0 @@
-select
-    training_exclusion_reason,
-    toUInt64(count()) as row_count
-from `fraudguard_ml`.`ml_training_candidates`
-group by training_exclusion_reason
